7bdfe8bcb5a008c785f91699dcc7426fac59bdba
CondeSun <michael.fritsch@bayformatic.de>
2025-06-25 14:38:15 +0200

    added untrusted cert support

--- i5-req/src/request/blocking.rs
+++ i5-req/src/request/blocking.rs
@@ -1,3 +1,5 @@
+use reqwest::blocking::Response;
+
 use crate::types::{
     i5_error::I5RequestError, i5_request::ValidatedI5Request, i5_request_url::I5RequestUrl,
 };
@@ -6,18 +8,18 @@ pub fn i5_http_post(
     valid_body: ValidatedI5Request,
     url: I5RequestUrl,
     allow_untrusted_cert: bool,
-) -> Result<(), I5RequestError> {
+) -> Result<Response, I5RequestError> {
     let body = valid_body.to_json_string()?;
     let client = reqwest::blocking::Client::builder()
         .danger_accept_invalid_certs(allow_untrusted_cert)
         .build()
         .map_err(I5RequestError::RequestError)?;
 
-    client
+    let res = client
         .post(url.to_url())
         .header("Conten-Type", "application/json")
         .body(body)
         .send()
         .map_err(I5RequestError::RequestError)?;
-    Ok(())
+    Ok(res)
 }
