7dbfb96ed0b1e3f6cfac1e0e6586fc0287878d4e
CondeSun <michael.fritsch@bayformatic.de>
2025-06-25 12:42:41 +0200

    added temp async Interface Request

--- i5-req/src/request/mod.rs
+++ i5-req/src/request/mod.rs
@@ -1,5 +1,21 @@
 pub mod blocking;
 
-pub async fn i5_http_post() {
-    todo!();
+use crate::types::{
+    i5_error::I5RequestError, i5_request::ValidatedI5Request, i5_request_url::I5RequestUrl,
+};
+
+pub async fn i5_http_post(
+    valid_body: ValidatedI5Request,
+    url: I5RequestUrl,
+) -> Result<(), I5RequestError> {
+    let body = valid_body.to_json_string()?;
+    let client = reqwest::Client::new();
+    client
+        .post(url.to_url())
+        .header("Conten-Type", "application/json")
+        .body(body)
+        .send()
+        .await
+        .map_err(|e| I5RequestError::RequestError)?;
+    Ok(())
 }
